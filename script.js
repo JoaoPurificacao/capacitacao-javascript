d884d50b9adc145292ec8002edef99fce5da00f4
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-05-05 14:44:31 -0300

    Operações básica em JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+// var x = 1
+let x = 10
+let y = 2
+const z = 3
+
+let soma = x + y
+let subtracao = y-x
+let multiplacao = x*z
+let divisao = z/x
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplacao)
+console.log(divisao)
