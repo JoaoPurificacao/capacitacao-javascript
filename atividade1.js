afe0b757955ca2357e53b08d1e8f87b7ff0fd60f
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-05-05 16:33:48 -0300

    Atividade 1

---
+++ atividade1.js
@@ -0,0 +1,6 @@
+let pessoa = {
+    altura: 1.89,
+    peso: 83
+    }
+
+// Demonstrar no console do navegador o IMC do objeto pessoa
--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // var x = 1
-let x = 10
-let y = 2
-const z = 3
+// let x = 10
+// let y = 2
+// const z = 3
 
-let soma = x + y
-let subtracao = y-x
-let multiplacao = x*z
-let divisao = z/x
+// let soma = x + y
+// let subtracao = y-x
+// let multiplacao = x*z
+// let divisao = z/x
 
-// console.log(soma)
-// console.log(subtracao)
-// console.log(multiplacao)
-// console.log(divisao)
+// // console.log(soma)
+// // console.log(subtracao)
+// // console.log(multiplacao)
+// // console.log(divisao)
 
-let palavra = "praxis" //String
-let verdadeiro = true //Boolean
-let falso = false //Boolean
+// let palavra = "praxis" //String
+// let verdadeiro = true //Boolean
+// let falso = false //Boolean
 
-// console.log(palavra)
-// console.log(verdadeiro)
-// console.log(falso)
+// // console.log(palavra)
+// // console.log(verdadeiro)
+// // console.log(falso)
 
-let casa = [] //array
-let andar4 = [123, 321, 213];
-andar4[3] = 456
+// let casa = [] //array
+
+// andar4[3] = 456
 
 // console.log(andar4[3])
 
@@ -32,9 +32,62 @@ andar4[3] = 456
 // >= é maior que igual
 // <= menor que igual
 
-let condicao = x + y >= 10
-console.log(condicao)
 
-if(condicao){
-    console.log("João")
+// let x = 10
+// let y = 2
+const z = 3
+let nome = "João Daneil"
+
+
+let nota =  4
+let condicao = nota > 6
+let condicaoDiferente = nota > 8
+
+
+// if(condicao){
+//     console.log(nome)
+// }else{
+    //     console.log("João é feio")
+    // }
+    
+// if (condicaoDiferente) {
+//     console.log("Nota muito boa")
+// } 
+// else if (condicao){
+//     console.log("Nota normal")
+// } else{
+//     console.log("Nota ruim")
+// }
+    
+// console.log(condicao)
+// let andar4 = [123, 321, 213]
+
+// andar4[3] = 2000
+
+// for(let i = 0; i < nome.length; i++){
+//     console.log(nome[i])
+// 
+
+
+function Soma (x, y){
+    let soma = x + y
+    console.log(typeof x)
+    console.log(typeof y)    
+    console.log(soma);
 }
+
+// Soma(100, 200)
+
+
+
+let numero =  "10" + 10
+
+
+// Soma("João", "Daniel")
+// Soma(nota, z)
+// Soma(nota, nome)
+
+console.log(numero)
+console.log(typeof numero)
+// console.log(typeof nome)
+
