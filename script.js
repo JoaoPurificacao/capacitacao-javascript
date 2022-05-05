771232a1d0aebd3063e4da80cc2c0d9b1b434a1f
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-05-05 15:02:36 -0300

    Array

--- script.js
+++ script.js
@@ -8,7 +8,22 @@ let subtracao = y-x
 let multiplacao = x*z
 let divisao = z/x
 
-console.log(soma)
-console.log(subtracao)
-console.log(multiplacao)
-console.log(divisao)
+// console.log(soma)
+// console.log(subtracao)
+// console.log(multiplacao)
+// console.log(divisao)
+
+let palavra = "praxis" //String
+let verdadeiro = true //Boolean
+let falso = false //Boolean
+
+console.log(palavra)
+console.log(verdadeiro)
+console.log(falso)
+
+let casa = [] //array
+let andar4 = [123, 321, 213];
+andar4[3] = 456
+
+console.log(andar4[3])
+
