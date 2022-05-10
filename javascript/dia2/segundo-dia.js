2e66b05c56de8e27569083c64f3ed9683d69df11
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-05-10 15:22:17 -0300

    Adicionando elementos no HTML via Javascript

--- javascript/dia2/segundo-dia.js
+++ javascript/dia2/segundo-dia.js
@@ -1,11 +1,22 @@
-let inputCores = document.getElementById('cores'); //Peguei o input
-let titulo = document.getElementsByTagName('h1'); //Peguei os H1
+// let inputCores = document.getElementById('cores'); //Peguei o input
+// let titulo = document.getElementsByTagName('h1'); //Peguei os H1
 
-let botao = document.getElementById('botao__mudar')
+// let botao = document.getElementById('botao__mudar')
 
-function trocandoCores(){
-    titulo[0].style.color = inputCores.value
-}
+// function trocandoCores(){
+//     titulo[0].style.color = inputCores.value
+// }
 
-// inputCores.addEventListener('input',trocandoCores)
-botao.addEventListener('click', trocandoCores)
+// // inputCores.addEventListener('input',trocandoCores)
+// botao.addEventListener('click', trocandoCores)
+
+let div = document.getElementsByTagName('div');
+
+//div[0].innerHTML +=  '<input type="text">'
+
+let novoInput = document.createElement('input');
+novoInput.setAttribute('maxlength', '14')
+novoInput.setAttribute('placeholder', 'CPF')
+novoInput.setAttribute('id', 'input-novo')
+div[0].appendChild(novoInput)
+
